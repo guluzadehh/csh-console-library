ece6607b02c66dfd4703c5e05eec551945320bb9
Resul Quluzade <quluzade.resul13@mail.ru>
2024-02-09 20:43:33 +0400

    Add constructor to BaseException

--- Exceptions.cs
+++ Exceptions.cs
@@ -9,6 +9,10 @@ public class BaseException : Exception
     public BaseException(string? message) : base(message)
     {
     }
+
+    public BaseException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
 }
 
 public class QuitInputRead : BaseException
